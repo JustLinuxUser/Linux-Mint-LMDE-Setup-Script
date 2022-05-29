12a10c32e7119f85c2d31370fbc6acdeccb035c6
Andriy Dohniak <pavlofort@gmail.com>
2022-05-29 15:11:09 +0200

    Cleaned up

--- main.sh
+++ main.sh
@@ -4,13 +4,14 @@ source scripts/deps.sh
 export SUDO_ASKPASS=scripts/askpass
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 rm log
 
 run () {
    sudo -A -v
-   echo -e "${GREEN} RUNNING: ${NC} ${2}" | tee -a log
+   echo -e "${YELLOW} RUNNING: ${NC} ${2}" | tee -a log
    $1 &>> log
    if [ $? -eq 0 ]; then
    	echo -e "${GREEN} SUCCESS: ${NC} ${2}"
@@ -24,8 +25,8 @@ run () {
 }
 
 run "sudo apt-get update --yes" "Update package database"
-run "sudo apt-get upgrade --yes" "Upgrade install packages"
-run "sudo apt-get install $deps --yes" "Installing packages \(may take a long time\)"
+run "sudo apt-get upgrade --yes" "Upgrade installed packages"
+run "sudo apt-get install $deps --yes" "Install packages \(may take a long time\)"
 
 if $(cat /etc/locale.gen | grep -E "^uk_UA.UTF-8 UTF-8"); then
 	run 'echo "uk_UA.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen"' 'Adding ukrainian locale'
